57c9ecada987feed3b268c462e479401d6e98f0a
HarshDhiman <142768740+harshDhiman3344@users.noreply.github.com>
2025-07-11 13:56:48 +0530

    Removed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import Space from "./Components/SpaceMain/Space"
-import { Analytics } from "@vercel/analytics/next"
+
 
 
 const App = () => {
   return (
-    <div>
-      <Analytics/>
-      <Space/>
-      </div>
+    <div><Space/></div>
   )
 }
 
